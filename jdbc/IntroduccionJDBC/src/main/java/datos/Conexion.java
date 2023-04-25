68f811342d5b43421a108691407cda7d83ce3649
Alexis Alberto Masin <AlexisMasin86@gmail.com>
2023-04-25 16:15:01 -0300

    Pool de conexiones
    Agregamos el manejo de pool de conexiones con Apache Commons

--- jdbc/IntroduccionJDBC/src/main/java/datos/Conexion.java
+++ jdbc/IntroduccionJDBC/src/main/java/datos/Conexion.java
@@ -1,5 +1,8 @@
 package datos;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class Conexion {
@@ -8,8 +11,17 @@ public class Conexion {
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "admin";
 
+    //Configuracion de pool de conexiones para la aplicacion
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASS);
+        ds.setInitialSize(5);
+        return ds;
+    }
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+        return getDataSource().getConnection();
     }
 
     public static void close(ResultSet rs){
